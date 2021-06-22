97f1788f166be4d86b32e390790ff490bd5fc084
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-06-22 18:27:17 -0400

    Added a header.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, {useEffect, useState} from "react";
 import axios from 'axios'
 import "./App.css";
 
-
+import Header from "./components/Header";
 
 function App() {
-  
+  const [nasaData, setNasaData] = useState(null);
+
+  // useEffect(() => {
+  //   const fetchNasa = () => {
+  //     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  //     .then(request => {
+  //       setNasaData(request.data)
+  //     })
+  //     .catch(error => console.log(error));
+  //   }
+
+  //   fetchNasa();
+  // }, []);
+
+
   return (
-    <div></div>  
+    <div className="App">
+      <Header nasaData={nasaData}/>
+    </div>
   );
 }
 
