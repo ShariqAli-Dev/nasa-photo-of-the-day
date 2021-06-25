f8dd87f89edc6e03f93a31f9603904bbee786503
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-06-24 21:32:02 -0400

    Changed Colors In Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledH1 = styled.h1`
-  color: #061f4a;
-  border: dotted white 12px;
-  padding: 10px;
+const StyledDiv = styled.div`
+  h1 {
+    color: #061f4a;
+    border: dotted white 12px;
+    padding: 10px;
+  }
+
+  #dateP {
+    font-weight: bold;
+    color: #323a45;
+  }
 `;
 
 export default function Header(props) {
   const { nasaData } = props;
   const { date } = nasaData;
   return (
-    <div>
-      <StyledH1>Nasa's Image Of The Day!</StyledH1>
+    <StyledDiv>
+      <h1>Nasa's Image Of The Day!</h1>
       <p>Every day this website will have updated information about the image, name, date, and explanation of the topic. Everything is aimed into showing something new and interesting to whoever chooses to visit this</p>
-      <p>{date}</p>
-    </div>
+      <p id="dateP">{date}</p>
+    </StyledDiv>
   );
 }
